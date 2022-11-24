01f766013c1aa577f21d9874897aab66b25acbc4
yuki.mabuchi <mbyuki.0408@gmail.com>
2022-11-24 21:44:48 +0900

    getAllMessagesを追加

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -7,13 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "getAllTasks", query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC")
+})
 @Table(name = "tasks")
 public class Tasks {
     @Id
-    @Column(name="id")
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
@@ -57,8 +62,5 @@ public class Tasks {
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
-
-
-
 
 }
